f03f239c23f83e20c0c22b89aa55f1a6d8c08865
JasonGuo <geqianci@163.com>
2017-02-21 10:51:36 +0800

    add article 41(2)

--- Apps.Admins/Controllers/SysStructController.cs
+++ Apps.Admins/Controllers/SysStructController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Apps.Admins.Core;
+using Apps.Common;
+using Apps.IBLL;
+using Apps.Models.Sys;
+using Microsoft.Practices.Unity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +11,152 @@ using System.Web.Mvc;
 
 namespace Apps.Admins.Controllers
 {
-    public class SysStructController : Controller
+    public class SysStructController : BaseController
     {
+        [Dependency]
+        public ISysStructBLL m_BLL { get; set; }
+        ValidationErrors errors = new ValidationErrors();
         // GET: SysStruct
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public JsonResult GetList(GridPager pager, string queryStr)
+        {
+            List<SysStructModel> list = m_BLL.GetList(ref pager, queryStr);
+            var json = new
+            {
+                total = pager.totalRows,
+                rows = (from r in list
+                        select new SysStructModel()
+                        {
+
+                            Id = r.Id,
+                            Name = r.Name,
+                            ParentId = r.ParentId,
+                            Sort = r.Sort,
+                            Higher = r.Higher,
+                            Enable = r.Enable,
+                            Remark = r.Remark,
+                            CreateTime = r.CreateTime
+
+                        }).ToArray()
+
+            };
+
+            return Json(json);
+        }
+
+        #region 创建
+        [SupportFilter]
+        public ActionResult Create()
+        {
+            ViewBag.Perm = GetPermission();
+            return View();
+        }
+
+        [HttpPost]
+        [SupportFilter]
+        public JsonResult Create(SysStructModel model)
+        {
+            model.Id = ResultHelper.NewId;
+            model.CreateTime = ResultHelper.NowTime;
+            if (model != null && ModelState.IsValid)
+            {
+
+                if (m_BLL.Create(ref errors, model))
+                {
+                    LogHandler.WriteServiceLog(GetUserId(), "Id" + model.Id + ",Name" + model.Name, "成功", "创建", "SysStruct");
+                    return Json(JsonHandler.CreateMessage(1, Suggestion.InsertSucceed));
+                }
+                else
+                {
+                    string ErrorCol = errors.Error;
+                    LogHandler.WriteServiceLog(GetUserId(), "Id" + model.Id + ",Name" + model.Name + "," + ErrorCol, "失败", "创建", "SysStruct");
+                    return Json(JsonHandler.CreateMessage(0, Suggestion.InsertFail + ErrorCol));
+                }
+            }
+            else
+            {
+                return Json(JsonHandler.CreateMessage(0, Suggestion.InsertFail));
+            }
+        }
+        #endregion
+
+        #region 修改
+        [SupportFilter]
+        public ActionResult Edit(string id)
+        {
+            ViewBag.Perm = GetPermission();
+            SysStructModel entity = m_BLL.GetById(id);
+            return View(entity);
+        }
+
+        [HttpPost]
+        [SupportFilter]
+        public JsonResult Edit(SysStructModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+
+                if (m_BLL.Edit(ref errors, model))
+                {
+                    LogHandler.WriteServiceLog(GetUserId(), "Id" + model.Id + ",Name" + model.Name, "成功", "修改", "SysStruct");
+                    return Json(JsonHandler.CreateMessage(1, Suggestion.EditSucceed));
+                }
+                else
+                {
+                    string ErrorCol = errors.Error;
+                    LogHandler.WriteServiceLog(GetUserId(), "Id" + model.Id + ",Name" + model.Name + "," + ErrorCol, "失败", "修改", "SysStruct");
+                    return Json(JsonHandler.CreateMessage(0, Suggestion.EditFail + ErrorCol));
+                }
+            }
+            else
+            {
+                return Json(JsonHandler.CreateMessage(0, Suggestion.EditFail));
+            }
+        }
+        #endregion
+
+        #region 详细
+        [SupportFilter]
+        public ActionResult Details(string id)
+        {
+            ViewBag.Perm = GetPermission();
+            SysStructModel entity = m_BLL.GetById(id);
+            return View(entity);
+        }
+
+        #endregion
+
+        #region 删除
+        [HttpPost]
+        [SupportFilter]
+        public JsonResult Delete(string id)
+        {
+            if (!string.IsNullOrWhiteSpace(id))
+            {
+                if (m_BLL.Delete(ref errors, id))
+                {
+                    LogHandler.WriteServiceLog(GetUserId(), "Id:" + id, "成功", "删除", "SysStruct");
+                    return Json(JsonHandler.CreateMessage(1, Suggestion.DeleteSucceed));
+                }
+                else
+                {
+                    string ErrorCol = errors.Error;
+                    LogHandler.WriteServiceLog(GetUserId(), "Id" + id + "," + ErrorCol, "失败", "删除", "SysStruct");
+                    return Json(JsonHandler.CreateMessage(0, Suggestion.DeleteFail + ErrorCol));
+                }
+            }
+            else
+            {
+                return Json(JsonHandler.CreateMessage(0, Suggestion.DeleteFail));
+            }
+
+
+        }
+        #endregion
     }
 }
